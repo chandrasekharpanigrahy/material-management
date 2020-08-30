b5f48589564d0f33282b2148451c5c24ca8fb6ee
likhita sahu <engg.likhita.sahu@gmail.com>
2020-08-30 14:06:54 +0530

    changed class to function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
-  state = {materialName: ''};
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <form onSubmit={this.addMaterial}>
-              <input type="text" placeholder="Enter material name" value={this.state.materialName}/>
-              <button>
-                Add Material
-              </button>
-            </form>
-          </header>
-        </div>
-    );
-  }
+const App = () =>
+  (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <input type="text" placeholder="Enter material name"/>
+            <button>
+              Add Material
+            </button>
+          </form>
+        </header>
+      </div>
+  );
 
-  addMaterial(){
-
-  }
-}
 
 export default App;
