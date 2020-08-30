ed819ffdbc563f7025eccd36871add3da6991300
likhita sahu <chandrasekharpanigrahy@gmail.com>
2020-08-30 15:55:54 +0530

    created new component for Material

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Material from "./admin/components/Material";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Material />
   </React.StrictMode>,
   document.getElementById('root')
 );
